f75620cff2990192acffffda5e2792de31b6edb0
Md.Raihanul Islam Rafi <19-41790-3@student.aiub.edu>
2023-03-25 14:07:03 +0600

    k

---
+++ TierAppIntro/Controllers/EmpController.cs
@@ -0,0 +1,30 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TierAppIntro.Controllers
+{
+    
+    public class EmpController : ApiController
+    {
+        [HttpGet]
+        [Route("api/employees")]
+        public HttpResponseMessage Get() 
+        {
+            try 
+            {
+               return Request.CreateResponse(HttpStatusCode.OK, EmployeeService.Get());
+            }
+            catch(Exception ex)
+            {
+               return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+          
+        }
+
+    }
+}
